da1a4a2a4c413534ad7c0a67f43e722598543dc7
ChaianneRodrigues <crfrocha@ucs.br>
2025-04-26 18:07:47 -0300

    Update script.sql

--- server/db/script.sql
+++ server/db/script.sql
@@ -5,9 +5,10 @@ USE petmatch;
 CREATE TABLE IF NOT EXISTS usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(100) UNIQUE NOT NULL,
     senha VARCHAR(255) NOT NULL,
-    telefone VARCHAR(20),
+    telefone VARCHAR(15),
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     tipo_usuario ENUM('doador', 'adotante') NOT NULL
 );
 
